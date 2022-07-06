590d39d96e041a83285b7f8424017978ca446b42
liu2207 <lieng2018@163.com>
2022-07-06 09:54:23 +0800

    'commit 01'

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,17 @@
+/**
+ * 需求:使用idea操作git
+ *
+ *分析:
+ *  1 本地仓库初始化
+ *  2 本地仓库的相关操作
+ *      2.1 工作区--暂存区
+ *      2.2 暂存区--本地仓库
+ *      2.3 修改文件后的版本切换
+ *
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
